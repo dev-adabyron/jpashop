1c9c4b493ec0f72b6e8ecebd29bfb65e2fbd865e
dy <dev.adabyron@gmail.com>
2023-03-05 08:00:13 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column (name = "delivery_id")
+    private long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order orders;
+
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+}
